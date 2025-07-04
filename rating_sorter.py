8e9e74ede354fc22c2f0f5df956e6672fe60c985
Ozodjon <Ozodj.one.pro@gmail.com>
2025-07-04 06:59:42 +0500

    .

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,12 @@
+import csv
 
+with open('students.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    students = list(reader)
+
+students.sort(key=lambda x: int(x['score']), reverse=True)
+
+with open('rating.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['name', 'score'])
+    writer.writeheader()
+    writer.writerows(students)
